438ed6d134e3c14adfedde7fb277134c39785a44
maksem <maksem@debian>
2026-03-03 23:53:37 +0300

    concurrency

--- concurrency/bakery.go
+++ concurrency/bakery.go
@@ -1,5 +1,120 @@
 package concurrency
 
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"sync"
+	"time"
+)
+
+/*
+С помощью горутин и каналов смоделировать работу пекарни:
+
+Пекарня состоит из 2+1 этапов: торт выпекается, торт запаковывается, все торты смотрятся. Связь между
+этапами происходит через каналы, этапы выполняются с помощью горутин. Торт представляет из себя объект
+с полями типа int: BakedBy, BakeTime, PackedBy, PackTime Программа должна работать следующим образом:
+    Запускается N горутин, каждая из которых за время T1 = i +-t1 (i - номер рутины, t1 - выбранный
+	вами параметр) создаёт объект тортика, заполняет поля BackedBy=i, BakeTime=T1 и отправляет его в канал
+
+    Существует пул из M горутин. когда в канал приходят торитики с предыдущего этапа, свободная горутина
+	из пула начинает его упаковывать, то есть за время T2 = j+-t2 (j - номер рутины из пула,
+	t2 - выбранный вами параметр, причём t2>=t1) выставляет тортику параметры PackedBy=j,
+	PackTime = T2 и отправляет в канал
+
+    Мы ждём пока придут все тортики, или мы получим сигнал о завершении работы(тогда дожидаемся завершения
+	работы текущих рутин), после чего просто выводим тортики в порядке, в котором они пришли к нам. При
+	полном отрабатывании программы должно быть K тортиков.
+*/
+
 type Cake struct {
-	BackedBy, BackedTime, PackeddBy, PackedTime int
+	BakedBy int
+	BakedTime int
+	PackedBy int
+	PackedTime int
+}
+
+// messages about the Cakes Packed are stored in tmpConcurrency.log, while benchmarks are standard output
+func StartBakery(N, M, K int) {
+	start := time.Now()
+	const t1 = 10 // ms
+	const t2 = 15
+	
+	bakedCakes := make(chan Cake, N)
+	packedCakes := make(chan Cake, M)
+	
+	var bakersWG sync.WaitGroup
+	
+	bakersWG.Add(N)
+	for i := range N {
+		go func(bakerID int) {
+			defer bakersWG.Done()
+			var distributedCakes int // amount of cakes which should be done by this goroutine
+			if bakerID < K%N {
+				distributedCakes = K/N + 1
+			} else {
+				distributedCakes = K/N
+			}
+			for range distributedCakes {
+				variance := rand.Intn(2*t1+1) - t1
+				bakeTime := max(bakerID + variance, 5)
+
+				time.Sleep(time.Duration(bakeTime) * time.Millisecond)
+
+				cake := Cake{
+					BakedBy:   bakerID,
+					BakedTime: bakeTime,
+				}
+				bakedCakes <- cake
+			}
+		}(i)
+	}
+	
+	go func() {
+		bakersWG.Wait()
+		close(bakedCakes)
+	}()	
+
+	var packersWG sync.WaitGroup
+	packersWG.Add(M)
+	for i := range M {
+		go func(packerID int) {
+			defer packersWG.Done()
+			for cake := range bakedCakes {
+				variance := rand.Intn(2*t2+1) - t2
+				packTime := max(packerID + variance, 5)
+				
+				time.Sleep(time.Duration(packTime) * time.Millisecond)
+				
+				cake.PackedBy = packerID
+				cake.PackedTime = packTime
+				packedCakes <- cake
+			}
+		}(i)
+	}
+
+	go func() {
+		packersWG.Wait()
+		close(packedCakes)
+	}()
+
+	f, err := os.OpenFile("tmp_concurrency.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+	log.Printf("NEW LOG ENTRY on %v\n", time.Now())
+	
+	cakeNum := 1
+	for cake := range packedCakes {
+		log.Printf("Cake N%d: BakedBy: %d, BakedTime: %d, PackedBy: %d, PackedTime: %d\n", 
+			cakeNum, cake.BakedBy, cake.BakedTime, cake.PackedBy, cake.PackedTime)
+		cakeNum++
+	}
+	
+	f.Sync()
+	t := time.Now()
+	fmt.Printf("Bakery with params N = %v, M = %v, K = %v took %v\n", N, M, K, t.Sub(start))
 }
--- main.go
+++ main.go
@@ -1,43 +1,24 @@
 package main
 
 import (
-	"fmt"
 
-	"github.com/mmaxemm/internship_tasks/download"
+	"github.com/mmaxemm/internship_tasks/concurrency"
 )
 
-// ErrInvalidURL - можно вывести сообщение об ошибке и запросить у пользователя ввести действительный URL.
-// InvalidProtocolError -  если функция вернула эту ошибку, необходимо указать какой именно неверный протокол был указан в переданном пользователем URL и запросить ввести действительный URL.
-// ErrConnectionFailed - если функция вернула эту ошибку, можно вывести сообщение об ошибке и предложить пользователю повторить попытку или проверить свои настройки подключения.
-// ErrFileNotFound - если функция вернула эту ошибку, можно вывести сообщение об ошибке и запросить у пользователя проверить правильность URL или наличие файла на сервере.
-// ErrDownloadFailed - если функция вернула эту ошибку, можно вывести сообщение об ошибке и запросить у пользователя проверить доступность файла для скачивания.
-// В случае, если в функции произошла какая-то другая ошибка, которая не была описана выше, функция может вернуть стандартную ошибку Go ErrUnexpectedEOF или создать свою собственную ошибку с пояснением причины ошибки.
+/*
+Для данной задачи можно использовать различные наборы параметров, однако рекоммендуется попробовать
+следующие комбинации:
+	- K = 10_000, N = 1, M = 1
+	- K = 10_000, N = 8, M = 5
+	- K = 10_000, N = 100, M = 50
+*/
 
 func main() {
-	var fileName = "tmp.html"
-	var fileURL = "https://duckduckgo.com"
-	err := download.DownloadFile(fileURL, fileName)
-	if err == nil {
-		return
-	}
-	switch e := err.(type) {
-	case download.ErrInvalidURL:
-		fmt.Println(err.Error())
-	case download.InvalidProtocolError:
-		fmt.Println(err.Error())
-		fmt.Println("Please, enter URL with a valid protocol(http/https)")
-	case download.ErrConnectionFailed:
-		fmt.Println(err.Error())
-		fmt.Println("Please, try again or check network connection")
-	case download.ErrFileNotFound:
-		fmt.Println(err.Error())
-		fmt.Println("Please, check the URL or the existence of this file on the server")
-	case download.ErrDownloadFailed:
-		fmt.Println(err.Error())
-		fmt.Println("Please, check if the file is available")
-	default:
-		fmt.Printf("unexpected error: %v", e)
-	}
-
-
+	K := 10000
+	M1, N1 := 1, 1
+	M2, N2 := 5, 8
+	M3, N3 := 50, 100
+	concurrency.StartBakery(N1, K, M1)
+	concurrency.StartBakery(N2, K, M2)
+	concurrency.StartBakery(N3, K, M3)
 }
